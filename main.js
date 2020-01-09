9888cc71b4c014b3d33417304d31ea26e7dded6a
cosmian <softkey95@gmail.com>
2020-01-09 09:28:53 +0900

    Arrange whole code

---
+++ main.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const cronJob = require('cron').CronJob;
+const botkit = require('botkit');
+const controller = botkit.slackbot({
+	debug: false,
+	log: true
+});
+const bot = controller.spawn({
+	token: process.env.TOKEN
+    });
+const scrape = require('./scrape');
+
+// Set hearing option in this Array
+const botScope = [
+    'direct_message',
+    'direct_mention',
+    'mention',
+    'ambient'
+];
+
+// Add User in this Array
+const users = [
+    'well-balanced',
+    'indante',
+    'incleaf'
+];
+
+const sendMessages = async(messages) => {
+    for (var i = 0; i < messages.length; i++) {
+        bot.say({
+            text:messages[i],
+            channel: 'bots-playground'
+        })
+    }
+};
+
+const sendReplyPersonal = (username) => {
+    controller.hears(username,botScope, (bot,message)=>{
+        scrape.getCount(username)
+        .then(count => {
+            return scrape.formatCheckMessage(count)
+        })
+        .then(formattedMessage => {
+            bot.reply(message,formattedMessage)
+        })
+    })
+};
+
+const sendReply = (usernames) => {
+    usernames.forEach((user) => {
+        sendReplyPersonal(user)
+    })
+};
+
+// excute function
+// Time Setting : ex) 00 00 00 * * 1-5 : every Mon ~ Fri
+new cronJob('00 00 00 * * *',async()=>{
+    scrape.getCounts(users)
+    .then(counts => {
+    return scrape.formatFailMessages(counts,users)
+    })
+    .then(messages => {
+        sendMessages(messages)
+    })
+},null,true,"Asia/Seoul")
+
+sendReply(users);
+
+bot.startRTM(function(){
+    bot.say({
+    text: 'Greeny works!',
+    channel: 'bots-playground'
+    })
+});
--- scrape.js
+++ scrape.js
@@ -1,154 +1,73 @@
-require('dotenv').config();
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { IncomingWebhook } = require('@slack/webhook');
-const url = process.env.WebhookURL;
-const webhook = new IncomingWebhook(url);
-const schedule = require('node-schedule')
-const botkit = require('botkit')
-const Slack = require('slack-node')
-const token = process.env.TOKEN
-const controller = botkit.slackbot({
-	debug: false,
-	log: true
-});
-const bot = controller.spawn({
-	token: process.env.TOKEN
-    })
 
-
-const botScope = [
-    'direct_message',
-    'direct_mention',
-    'mention',
-    'ambient'
-]
-const userNames = ['well-balanced','indante','incleaf']
-const sendMessage = function(){
-    for (var i=0; i<userNames.length; i++) {
-        controller.hears(userNames[i],botScope, (bot,message)=>{
-            bot.reply(message,'ㅋㅋ')
-        })   
+// It's for match between tag element and Day number
+exports.convertNumber = () => {
+    var date = new Date;
+    var number = date.getDay()
+    if (number===7) {
+        number = 0
+    } else {
+        number += 1;
     }
-}
-sendMessage()
-
-    
-bot.startRTM(function(){
-        bot.say({
-        text: 'Greeny works!',
-        channel: 'bots-playground'
-    })
-});
-
-/*
-Date.prototype.yyyymmdd = function() // prototype 말고 다른 함수로 고안해 만들어볼것
-{
-    var yyyy = this.getFullYear().toString();
-    var mm = (this.getMonth() + 1).toString();
-    var dd = this.getDate().toString();
- 
-    return yyyy + '-' + (mm[1] ? mm : '0'+mm[0]) + '-' + (dd[1] ? dd : '0'+dd[0]);
+    return number
 }
 
-var today = new Date().yyyymmdd().toLocaleString('ko-KR', {
-    timeZone: 'Asia/Seoul'
-  });
-
-
-const getTodayCommit = async() => {
-    const usernames = ['well-balanced', 'incleaf', 'indante']
-    const storedNames = []
-    const counts = []
-    const target_urls = []
-    for (var i=0; i<usernames.length; i++){
-        username = usernames[i]
-        storedNames.push(username)
-        const target_url = `https://github.com/${username}`
-        target_urls.push(target_url)
-        const response = await axios.get(target_url,{
-            responseType:"arraybuffer",
+// These are to announce commit success or fail
+exports.getCounts = async(users) => {
+    var counts = [];
+    for (var i=0; i<users.length; i++) {
+        let GitHubURL = `https://github.com/${users[i]}`;
+        let html = await axios.request({
+            url: GitHubURL,
             headers:{
-                Cookie:"tz=Asia%2FSeoul"
+                Cookie: "tz=Asia%2FSeoul;"
             }
-        })
-        if (response.status==200){
-            const $ = cheerio.load(response.data)
-            for (var x=1; x<8; x++) {
-                data_date = $("g:nth-child(53) rect:nth-child("+x+")").attr("data-date")
-                if (data_date==today) {
-                    var count = $("g:nth-child(53) rect:nth-child("+x+")").attr("data-count")
-                    console.log(count)
-                    counts.push(count)
-                }
-            }    
-        } 
-    } return {
-        counts: counts,
-        storedNames: storedNames,
-        target_urls: target_urls
+        });
+        let $ = cheerio.load(html.data);
+        let number = this.convertNumber();
+        let count = $(`g:nth-child(53) > rect:nth-child(${number})`).attr("data-count");
+        counts.push(count)
     }
+    return await counts
 }
 
 
-function createMessage(count){
-    if (count>0){
-        return ('님이 잔디 심기에 성공하셨습니다! 🥳 예쁜 정원이 만들어지는 중 🌱');
-    } else if (count==0){
-        return ('님이 잔디 심기에 실패하셨습니다 😭 우리 조금만 더 힘내봐요 🤗')
+exports.formatFailMessages = async(counts,users) => {
+    var messages = [];
+    for (var i = 0; i<counts.length; i++) {
+        if (counts[i] == 0) {
+            message = users[i] + '님이 잔디 심기에 실패하셨습니다 😭 우리 조금만 더 힘내봐요 🤗';
+        } else {
+            message = users[i] + '님이 잔디 심기에 성공하셨습니다! 🥳 예쁜 정원이 만들어지는 중 🌱';
+        }
+        messages.push(message)
     }
+    return await messages
 }
 
 
-//schedule.scheduleJob('59 23 * * *',async()=>
-       // await 
-        getTodayCommit().then(function(todayCommit){
-            for (var i=0; i<todayCommit.storedNames.length; i++){
-                const name = todayCommit.storedNames[i]
-                const url = todayCommit.target_urls[i]
-                const formattedMessage = createMessage(todayCommit.counts[i])
-                const message = {
-                    "blocks": [
-                        {
-                            "type": "context",
-                            "elements": [
-                                {
-                                    "type": "mrkdwn",
-                                    "text": `*<${url}|${name}>${formattedMessage}*`
-                                }
-                            ]
-                        }
-                    ]
-                }
-                webhook.send(message)
-            }
-        })
-//)
+// These are for reply personal
 
-//schedule.scheduleJob('00 22 * * *',async()=>
-    //await 
-    // getTodayCommit().then(function(todayCommit){
-    //         for (var i=0; i<todayCommit.storedNames.length; i++){
-    //             const name = todayCommit.storedNames[i]
-    //             const url = todayCommit.target_urls[i]
-    //             const formattedMessage = createMessage(todayCommit.counts[i])
-    //             if (todayCommit.counts[i]==0){
-    //                 const message2 = {
-    //                     "blocks": [
-    //                         {
-    //                             "type": "context",
-    //                             "elements": [
-    //                                 {
-    //                                     "type": "mrkdwn",
-    //                                     "text": `*<${url}|${name}>님은 아직 정원을 가꾸지 못하셨어요. 시간이 얼마 남지 않았답니다 😭*`
-    //                                }
-    //                             ]
-    //                       }
-    //                    ]
-    //                 }
-    //                 webhook.send(message2)
-    //           }
-    //      }
-    // })
-//)
-*/
+exports.getCount = async(username) => {
+    let GitHubURL = `https://github.com/${username}`;
+    let html = await axios.request({
+        url: GitHubURL,
+        headers:{
+            Cookie: "tz=Asia%2FSeoul;"
+        }
+    });
+    let $ = cheerio.load(html.data);
+    let number = this.convertNumber();
+    let count = $(`g:nth-child(53) > rect:nth-child(${number})`).attr("data-count");
+    return await count
+}
+
+exports.formatCheckMessage = async(count) => {
+        if (count == 0) {
+            message = '아직 잔디를 심지 않으셨어요 😭 우리 조금만 더 힘내봐요 🤗';
+        } else {
+            message = '잔디를 심으셨군요 🥳 초록초록하게 더 심어주세요 🌱';
+        }
+    return await message
+}
