dfecb3a9d20028ecb95427df12edbf02525348fe
well-balanced <softkey95@gmail.com>
2020-01-30 03:52:41 +0900

    Apply ESlint

--- main.js
+++ main.js
@@ -1,80 +1,75 @@
 require('dotenv').config();
-const cronJob = require('cron').CronJob;
+const { CronJob } = require('cron');
 const botkit = require('botkit');
-const controller = botkit.slackbot({
-	debug: false,
-	log: true
-});
+
+const controller = botkit.slackbot(
+  {
+    debug: false,
+    log: true,
+  },
+);
 const bot = controller.spawn({
-    retry: true,
-	token: process.env.TOKEN
-    });
+  retry: true,
+  token: process.env.TOKEN,
+});
 const scrape = require('./scrape');
 
 // Set hearing option in this Array
 const botScope = [
-    'direct_message',
-    'direct_mention',
-    'mention',
-   // 'ambient'
+  'direct_message',
+  'direct_mention',
+  'mention',
+// 'ambient'
 ];
 
 // Add User in this Array
 const users = [
-    'well-balanced',
-    'indante',
-    'incleaf',
-    'tkdals5429',
+  'well-balanced',
+  'indante',
+  'incleaf',
+  'tkdals5429',
 ];
 
-const sendMessages = async(messages) => {
-    for (var i = 0; i < messages.length; i++) {
-        bot.say({
-            text:messages[i],
-            channel: 'bots-playground'
-        })
-    }
+const sendMessages = async (messages) => {
+  for (let i = 0; i < messages.length; i++) {
+    bot.say({
+      text: messages[i],
+      channel: 'bots-playground',
+    });
+  }
 };
 
 const sendReplyPersonal = (username) => {
-    controller.hears(username,botScope, (bot,message)=>{
-        scrape.getCount(username)
-        .then(count => {
-            return scrape.formatCheckMessage(count)
-        })
-        .then(formattedMessage => {
-            bot.reply(message,formattedMessage)
-        })
-    })
+  controller.hears(username, botScope, (botAPI, message) => {
+    scrape.getCount(username)
+      .then((count) => scrape.formatCheckMessage(count))
+      .then((formattedMessage) => botAPI.reply(message, formattedMessage));
+  });
 };
 
 const sendReply = (usernames) => {
-    usernames.forEach((user) => {
-        sendReplyPersonal(user)
-    })
+  usernames.forEach((user) => sendReplyPersonal(user));
 };
 
-// excute function
+// execute function
 // Time Setting : ex) 00 00 00 * * 1-5 : every Mon ~ Fri
-new cronJob('00 00 00 * * *',async()=>{
-    scrape.getCounts(users)
-    .then(counts => {
-    return scrape.formatFailMessages(counts,users)
-    })
-    .then(messages => {
-        sendMessages(messages)
-    })
-},null,true,"Asia/Seoul")
+// eslint-disable-next-line no-new
+new CronJob('00 59 23 * * *', async () => {
+  scrape.getCounts(users)
+    .then((counts) => scrape.formatFailMessages(counts, users))
+    .then((messages) => {
+      sendMessages(messages);
+    });
+}, null, true, 'Asia/Seoul');
 
 sendReply(users);
 
-bot.startRTM(function(err){
-    if (err) {
-        console.log('Failed to start RTM');
-        return setTimeout(start)
-    }
-    bot.say({
+bot.startRTM((err) => {
+  if (err) {
+    throw Error('Failed to start RTM');
+  }
+  bot.say({
     text: 'Greeny works!',
-    channel: 'bots-playground'
-    })
+    channel: 'bots-playground',
+  });
 });
--- scrape.js
+++ scrape.js
@@ -3,75 +3,75 @@ const cheerio = require('cheerio');
 
 // It's for match between tag element and Day number
 exports.convertNumber = () => {
-    var date = new Date;
-    var number = date.getDay()
-    if (number===7) {
-        number = 0
-    } else {
-        number += 1;
-    }
-    return number
-}
+  const date = new Date();
+  let number = date.getDay();
+  if (number === 7) {
+    number = 0;
+  } else {
+    number += 1;
+  }
+  return number;
+};
 
 // These are to announce commit success or fail
-exports.getCounts = async(users) => {
-    var counts = [];
-    for (var i=0; i<users.length; i++) {
-        let GitHubURL = `https://github.com/${users[i]}`;
-        let html = await axios.request({
-            url: GitHubURL,
-            headers:{
-                Cookie: "tz=Asia%2FSeoul;"
-            }
-        });
-        let $ = cheerio.load(html.data);
-        let number = this.convertNumber();
-        let count = $(`g:nth-child(53) > rect:nth-child(${number})`).attr("data-count");
-        counts.push(count)
-    }
-    return await counts
-}
+exports.getCounts = async (users) => {
+  const counts = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < users.length; i++) {
+    const GitHubURL = `https://github.com/${users[i]}`;
+    // eslint-disable-next-line no-await-in-loop
+    const html = await axios.request({
+      url: GitHubURL,
+      headers: {
+        Cookie: 'tz=Asia%2FSeoul;',
+      },
+    });
+    const $ = cheerio.load(html.data);
+    const number = this.convertNumber();
+    const count = Number($(`g:nth-child(53) > rect:nth-child(${number})`).attr('data-count'));
+    counts.push(count);
+  }
+  return counts;
+};
 
 
-exports.formatFailMessages = async(counts,users) => {
-    var messages = [];
-    for (var i = 0; i<counts.length; i++) {
-        if (counts[i] == 0) {
-            message = users[i] + ' failed to plant grass today 😭';
-        } else {
-<<<<<<< HEAD
-            message = users[i] + ' succeeded to plant grass today 🥳 Beautiful GARDEN is being built 🌱';
-=======
-            message = users[i] + ' succeeded to plant glass today 🥳 Beautiful GARDEN is being built 🌱';
->>>>>>> 2e6a90ae210e552b6bf22291f703a5c8db26cbee
-        }
-        messages.push(message)
+exports.formatFailMessages = async (counts, users) => {
+  const messages = [];
+  let message;
+  for (let i = 0; i < counts.length; i++) {
+    if (counts[i] === 0) {
+      message = `${users[i]} failed to plant grass today 😭`;
+    } else {
+      message = `${users[i]} succeeded to plant grass today 🥳 Beautiful GARDEN is being built 🌱`;
     }
-    return await messages
-}
+    messages.push(message);
+  }
+  return messages;
+};
 
 
 // These are for reply personal
 
-exports.getCount = async(username) => {
-    let GitHubURL = `https://github.com/${username}`;
-    let html = await axios.request({
-        url: GitHubURL,
-        headers:{
-            Cookie: "tz=Asia%2FSeoul;"
-        }
-    });
-    let $ = cheerio.load(html.data);
-    let number = this.convertNumber();
-    let count = $(`g:nth-child(53) > rect:nth-child(${number})`).attr("data-count");
-    return await count
-}
+exports.getCount = async (username) => {
+  const GitHubURL = `https://github.com/${username}`;
+  const html = await axios.request({
+    url: GitHubURL,
+    headers: {
+      Cookie: 'tz=Asia%2FSeoul;',
+    },
+  });
+  const $ = cheerio.load(html.data);
+  const number = this.convertNumber();
+  const count = Number($(`g:nth-child(53) > rect:nth-child(${number})`).attr('data-count'));
+  return count;
+};
 
-exports.formatCheckMessage = async(count) => {
-        if (count == 0) {
-            message = ' has not planted grass today yet 😭 Go for it 🤗';
-        } else {
-            message = ' has already planted grass today 🥳 Plant more green 🌱';
-        }
-    return await message
-}
+exports.formatCheckMessage = async (count) => {
+  let message;
+  if (count === 0) {
+    message = ' has not planted grass today yet 😭 Go for it 🤗';
+  } else {
+    message = ' has already planted grass today 🥳 Plant more green 🌱';
+  }
+  return message;
+};
