8840e960891fae5fdef1067f0a22eed5413fc77e
well-balanced <ws.kim@specupad.com>
2020-07-31 02:16:13 +0900

    Remove built folder

--- built/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTimezoneCookie = void 0;
-exports.getTimezoneCookie = function (location) {
-    var continent = location.split("/")[0];
-    var city = location.split("/")[1];
-    var timezoneCookie = "tz=" + continent + "%2F" + city + ";";
-    return timezoneCookie;
-};
